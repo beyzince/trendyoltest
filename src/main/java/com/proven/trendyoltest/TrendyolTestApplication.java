4b6ff06245eb3bb6a3a1809198d30b9c6ca3ed10
arpanet <beyzaa0204@gmail.com>
2021-08-16 14:03:39 +0300

    replace maven project with spring boot

---
+++ src/main/java/com/proven/trendyoltest/TrendyolTestApplication.java
@@ -0,0 +1,13 @@
+package com.proven.trendyoltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendyolTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendyolTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proven/trendyoltest/TrendyolTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proven.trendyoltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendyolTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
